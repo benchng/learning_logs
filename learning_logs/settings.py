4cf498d144ef39e71f39ab09fdad05a38561b6da
Ben <ngben057@gmail.com>
2019-03-29 16:40:15 +0800

    update 7

--- learning_logs/settings.py
+++ learning_logs/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'learning_logs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_user_password',
+        'HOST': 'localhost',
+        'PORT': 'db_port_number',
     }
 }
 
